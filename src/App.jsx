18994edb6735605a4659657c3d54379541b2fc32
SardiusJay <abidoyejay@gmail.com>
2023-06-21 11:57:30 +0100

    link pages together

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import './App.css'
 import Nav from './components/Nav/Nav';
 import Hero from './components/Hero/Hero';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup'
+import About us from './components/About us/About'
+import Review from './components/Review/Review';
 
 function App() {
 
@@ -9,6 +13,10 @@ function App() {
     <>
       <Nav />
       <Hero/>
+      <Login />
+      <Signup />
+      <About us />
+      <Review />
     </>
   )
 }
